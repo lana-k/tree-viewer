755e242b18216b4d87cfca0010a5009df9c735d9
lana-k <skochetova89@gmail.com>
2024-07-13 12:39:01 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import TreeViewer from './components/TreeViewer.vue'
+import NodeCard from './components/NodeCard.vue'
+import { reactive } from 'vue'
+const selectedNodes = reactive([])
+</script>
+
+<template>
+  <div id="app-containner">
+    <aside>
+      <node-card 
+        v-for="node in selectedNodes" 
+        :key="node.name"
+        :node="node"
+      />
+    </aside>
+    <div id="main-container">
+      <header>  
+      </header>
+      
+      <main>
+        <tree-viewer />
+      </main>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#app-containner {
+  display: flex;
+}
+
+#main-container {
+  flex-grow: 1;
+}
+</style>
---
+++ src/components/NodeCard.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  node: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="node-card">
+    <h1 class="green">{{ node.name }}</h1>
+    <p>{{ node.description }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
