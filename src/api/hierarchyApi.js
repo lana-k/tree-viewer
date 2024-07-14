bbec0ae693823a872f94a93fedeb30caaff0c603
lana-k <skochetova89@gmail.com>
2024-07-14 13:15:04 +0200

    change api so it makes get request

--- src/api/hierarchyApi.js
+++ src/api/hierarchyApi.js
@@ -1,15 +1,6 @@
-import dummyData from './dummyData.json';
-
-const msMin = 100;
-const msMax = 2000;
-
 export default {
-  getHierarchy: () => new Promise(resolve => {
-    // Imitate response delay
-    const ms = Math.random() * (msMax - msMin) + msMin;
-
-    setTimeout(() => {
-      resolve(dummyData)
-    }, ms);
-  }),
-};
+  async getHierarchy() {
+    const res = await fetch('/dummyData.json')
+    return res.json()
+  }
+}
